26603597c3803ab4bb9042219e3527809c611cbd
Rob Hess <hessro@oregonstate.edu>
2024-01-24 14:56:17 -0800

    Add code from 1/24 lecture.

--- src/GeneralPurposeSelectApp.jsx
+++ src/GeneralPurposeSelectApp.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react"
+
 const starWarsOptions = {
   rey: "Rey",
   luke: "Luke Skywalker",
   leia: "Leia Organa",
   finn: "Finn (FN-2187)",
-  kylo: "Kylo Ren"
+  kylo: "Kylo Ren",
+  yoda: "Yoda"
 }
 
 const petOptions = {
@@ -13,17 +16,56 @@ const petOptions = {
   iguana: "Iguana"
 }
 
+function DropdownMenu(props) {
+  // const options = props.options
+  const { options, selection, onSelectionChange } = props
+  return (
+    <select value={selection} onChange={e => {
+      onSelectionChange(e.target.value)
+    }}>
+      {Object.keys(options).map(key => (
+        <option key={key} value={key}>{options[key]}</option>
+      ))}
+    </select>
+  )
+}
+
 
 export default function App() {
+  const [ starWarsSelection, setStarWarsSelection ] = useState(Object.keys(starWarsOptions)[0])
+  const [ petSelection, setPetSelection ] = useState(Object.keys(petOptions)[0])
+
+  console.log("== starWarsSelection:", starWarsSelection)
+  console.log("== petSelection:", petSelection)
+
+  // const numbers = [ 1, 2, 333, 4, 5 ]
+  // const squares = numbers.map(num => num * num)
+  // console.log("== squares:", squares)
   return (
-    <form>
-      <select>
+    <form onSubmit={e => {
+      e.preventDefault()
+      alert(`${starWarsOptions[starWarsSelection]} loves their ${petOptions[petSelection]}`)
+    }}>
+      {/* <ul>
+        {numbers.map(num => <li key={num}>{num * num}</li>)}
+      </ul> */}
+      <DropdownMenu
+        selection={starWarsSelection}
+        options={starWarsOptions}
+        onSelectionChange={selection => setStarWarsSelection(selection)}
+      />
+      <DropdownMenu
+        selection={petSelection}
+        options={petOptions}
+        onSelectionChange={selection => setPetSelection(selection)}
+      />
+      {/* <select>
         <option value="rey">Rey</option>
         <option value="luke">Luke Skywalker</option>
         <option value="leia">Leia Organa</option>
         <option value="finn">Finn (FN-2187)</option>
         <option value="kylo">Kylo Renn</option>
-      </select>
+      </select> */}
       <button type="submit">Submit</button>
     </form>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 // import App from './HandlingEventsApp'
-import App from './ControlledComponentsApp'
-// import App from './GeneralPurposeSelectApp'
+// import App from './ControlledComponentsApp'
+import App from './GeneralPurposeSelectApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
